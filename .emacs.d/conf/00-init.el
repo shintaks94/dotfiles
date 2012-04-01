6473771be53a5ed62b6bb1f791e626f346a1bc2f
Yousuke Shintaku <y.shintaku0617@gmail.com>
2012-04-01 12:49:30 +0900

    commit4

---
+++ .emacs.d/conf/00-init.el
@@ -0,0 +1,19 @@
+;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;;
+;; 00-init.el
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;;  Basic Settings  ;;
+;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; 環境毎の設定
+
+;;; ロケールの設定
+(set-language-environment 'Japanese)
+;;; 文字コードの設定
+(prefer-coding-system 'utf-8)
+
+;;; モードラインの設定
+(line-number-mode 0)                    ;行番号を表示しない
+(column-number-mode 0)                  ;桁番号を表示しない
+
